384be38878ac6603bf278390fe5e6bdd2b72a6f0
Kwabena N Amponsah <kwabenantim@yahoo.com>
2022-10-06 09:22:58 +0100

    HoneyCombMeshGenerator wrappers

---
+++ wrappers/HoneyCombMeshGenerator.wrap.cpp
@@ -0,0 +1,39 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <set>
+#include <vector>
+#include <string>
+#include <map>
+#include "SmartPointers.hpp"
+#include "UblasIncludes.hpp"
+#include "SharedHoneycombVertexMeshGenerator.hpp"
+
+#include "HoneycombVertexMeshGenerator.wrap.hpp"
+
+namespace py = pybind11;
+typedef SharedHoneycombVertexMeshGenerator HoneycombVertexMeshGenerator;
+PYBIND11_DECLARE_HOLDER_TYPE(T, boost::shared_ptr<T>);
+typedef ::boost::shared_ptr<MutableVertexMesh<2, 2> > _boost_shared_ptr_lt_MutableVertexMesh_lt_2_2_gt__gt_;
+
+class HoneycombVertexMeshGenerator_Overloads : public HoneycombVertexMeshGenerator{
+    public:
+    using HoneycombVertexMeshGenerator::SharedHoneycombVertexMeshGenerator;
+    ::boost::shared_ptr<MutableVertexMesh<2, 2> > GetMesh() override {
+        PYBIND11_OVERLOAD(
+            _boost_shared_ptr_lt_MutableVertexMesh_lt_2_2_gt__gt_,
+            HoneycombVertexMeshGenerator,
+            GetMesh,
+            );
+    }
+
+};
+void register_HoneycombVertexMeshGenerator_class(py::module &m){
+py::class_<HoneycombVertexMeshGenerator , HoneycombVertexMeshGenerator_Overloads , boost::shared_ptr<HoneycombVertexMeshGenerator >   >(m, "HoneycombVertexMeshGenerator")
+        .def(py::init<unsigned int, unsigned int, bool, double, double, double >(), py::arg("numElementsAcross"), py::arg("numElementsUp"), py::arg("isFlatBottom") = false, py::arg("cellRearrangementThreshold") = 0.01, py::arg("t2Threshold") = 0.001, py::arg("elementArea") = 0.5 * sqrt(3.))
+        .def(py::init< >())
+        .def(
+            "GetMesh", 
+            (::boost::shared_ptr<MutableVertexMesh<2, 2> >(HoneycombVertexMeshGenerator::*)()) &HoneycombVertexMeshGenerator::GetMesh, 
+            " "  )
+    ;
+}
---
+++ wrappers/HoneyCombMeshGenerator.wrap.hpp
@@ -0,0 +1,6 @@
+#ifndef HoneycombMeshGenerator_wrap_hpp
+#define HoneycombMeshGenerator_wrap_hpp
+
+namespace py = pybind11;
+void register_HoneycombMeshGenerator_class(py::module &m);
+#endif // HoneycombMeshGenerator_wrap_hpp
